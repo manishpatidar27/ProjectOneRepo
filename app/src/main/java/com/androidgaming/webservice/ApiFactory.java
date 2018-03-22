f7830378bb4c830fe450767779a677f3eaaa4754
manish <manishcodiant@gmail.com>
2018-03-22 17:42:16 +0530

    added a new directory

--- app/src/main/java/com/androidgaming/webservice/ApiFactory.java
+++ app/src/main/java/com/androidgaming/webservice/ApiFactory.java
@@ -5,9 +5,20 @@ package com.androidgaming.webservice;
 
 
 
+import android.content.Context;
+import android.util.Log;
+
+import com.androidgaming.helper.Constants;
+import com.androidgaming.helper.CustomPreference;
+import com.androidgaming.helper.PreferenceKeys;
+
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -19,6 +30,7 @@ public class ApiFactory {
 
     public static final String BASE_URL = "http://patrongaming.xindevserver.com/";
     private static Retrofit retrofit = null;
+    private static Retrofit retrofitWithHeader = null;
 
     public static Retrofit getRetrofitClient() {
 
@@ -40,5 +52,69 @@ public class ApiFactory {
         }
         return retrofit;
     }
+
+
+
+
+    public static Retrofit getRetrofitClientWithHeader(final Context context) {
+
+
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+
+        httpClientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Interceptor.Chain chain) throws IOException {
+                Request original = chain.request();
+
+                Request.Builder requestBuilder = original.newBuilder();
+
+//                if (! CustomPreference.getInstance(context).getValue(PreferenceKeys.API_TOKEN).equals("")) {
+//                    requestBuilder.addHeader("access-token",  CustomPreference.getInstance(context).getValue(PreferenceKeys.API_TOKEN));
+//                }
+
+                requestBuilder.addHeader("accept", "application/json");
+                requestBuilder.addHeader("Content-Type", "application/json");
+                Request request = requestBuilder.build();
+
+                return chain.proceed(request);
+            }
+        });
+
+
+        httpClientBuilder.connectTimeout(5, TimeUnit.MINUTES);
+        httpClientBuilder.readTimeout(5, TimeUnit.MINUTES);
+        httpClientBuilder.writeTimeout(5, TimeUnit.MINUTES);
+
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClientBuilder.addInterceptor(loggingInterceptor);
+
+        List<Interceptor> list = httpClientBuilder.interceptors();
+
+        for (int i = 0; i < list.size(); i++) {
+
+            Log.e(Constants.TAG_LOGCAT, "INTERCEPTOR FOR POSITION " + i + " " + list.get(i).getClass());
+
+
+        }
+
+        if (retrofitWithHeader == null) {
+            retrofitWithHeader = new Retrofit.Builder().baseUrl(BASE_URL).client(httpClientBuilder.build()).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofitWithHeader;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
 
 }
