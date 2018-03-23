f8c4d55a8960304020c778711148d00f03c98a54
manish <manishcodiant@gmail.com>
2018-03-23 20:34:11 +0530

    added a new directory

---
+++ app/src/main/java/com/androidgaming/fragment/TabFragment1.java
@@ -0,0 +1,21 @@
+package com.androidgaming.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androidgaming.R;
+
+/**
+ * Created by Manish Patidar on 23-03-2018.
+ */
+
+public class TabFragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_1, container, false);
+    }
+}
