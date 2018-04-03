c2b4d2f99f702eb8d810e1f2dd0c0f1d8f9d632d
manish <manishcodiant@gmail.com>
2018-04-03 13:56:10 +0530

    added a new directory

---
+++ app/src/main/java/com/androidgaming/activities/GraphInScroll.java
@@ -0,0 +1,19 @@
+package com.androidgaming.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.androidgaming.R;
+
+/**
+ * Created by Manish Patidar on 03-04-2018.
+ */
+public class GraphInScroll  extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph_in_scroll);
+    }
+}
